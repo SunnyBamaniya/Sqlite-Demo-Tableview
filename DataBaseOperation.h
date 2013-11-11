94c7ae75a6cc7c42f00ec1e7f55bb8679d703245
SunnyBamaniya <sunny.bamaniya1990@gmail.com>
2013-11-11 19:22:21 +0530

    Create DataBaseOperation.h

---
+++ DataBaseOperation.h
@@ -0,0 +1,5 @@
++(void)check_Create_DB;
++(BOOL)updateCategory:(NSString *)id playername:(NSString*)playername teamname:(NSString*)teamname;
++(NSMutableArray*)Selectplayer;
++(BOOL)Insert:(NSString *)playername teamname:(NSString *)teamname;
++(BOOL)DeleteUser:(NSString*)id;
